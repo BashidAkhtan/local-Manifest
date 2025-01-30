ea435e99318c3e36d327fa77686bd219c1a8aa30
Rakhshan Ali <121305132+Rakhshan7070@users.noreply.github.com>
2025-01-30 18:06:41 +0530

    miami: Initial crDroid setup

--- crDroid.sh
+++ crDroid.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # Initialize manifest
-repo init -u https://github.com/Project-Mist-OS/manifest -b 15 --git-lfs --depth=1
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs --depth=1
 
 # Remove if any previous clones
-
 
 # repo sync
 /opt/crave/resync.sh
 
 # Miami-Trees
 # Device
-git clone https://github.com/BashidAkhtan/android_device_motorola_miami.git -b 15.0_Mist device/motorola/miami
+git clone https://github.com/Motorola-Miami/android_device_motorola_miami.git -b 15.0_Mist device/motorola/miami
 # Device Common
 git clone https://github.com/Motorola-Miami/android_device_motorola_sm6375-common.git -b rebase/15.0 device/motorola/sm6375-common
 # Vendor 
@@ -24,16 +23,14 @@ git clone https://github.com/Motorola-Miami/android_kernel_motorola_sm6375.git -
 git clone https://github.com/Motorola-Miami/android_hardware_motorola.git -b 15.0 hardware/motorola
 # OTA Updater
 rm -rf packages/apps/Updater
-git clone https://github.com/Motorola-Miami/android_miami_packages_apps_Updater.git -b 15.0_Mist packages/apps/Updater
-
-# set build environment
-. build/envsetup.sh
+git clone https://github.com/Motorola-Miami/android_miami_packages_apps_Updater.git -b 15.0_crDroid packages/apps/Updater
 
 # Build Signing
 git clone https://github.com/BashidAkhtan/miami_build_signing.git vendor/
 
-#Preparing device
-mistify miami
+# set build environment
+. build/envsetup.sh
 
-# lunch
-mist b
+# Start Building
+brunch miami
+
